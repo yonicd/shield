00ab606f46b2a95edb259698b5283d9cd97e4f77
yonicd <yonicd@gmail.com>
2023-11-30 10:21:17 -0500

    Update uri_utils.R
    update make_gh_uri to support different branches

--- R/uri_utils.R
+++ R/uri_utils.R
@@ -1,9 +1,9 @@
 
 make_cran_uri <- function(dep, endpoint = 'api.github.com'){
-  sprintf('https://%s/repos/cran/%s/contents/DESCRIPTION',endpoint, dep)
+  sprintf('https://%s/repos/cran/%s/contents/DESCRIPTION', endpoint, dep)
 }
 
-make_gh_uri <- function(dep, endpoint = 'api.github.com'){
-  sprintf('https://%s/repos/%s/commits/master',endpoint,dep)
+make_gh_uri <- function(dep, endpoint = 'api.github.com', suffix = 'main'){
+  sprintf('https://%s/repos/%s/commits/%s', endpoint, dep, suffix)
 }
 
